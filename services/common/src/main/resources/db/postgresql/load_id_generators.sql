82e87e385f134d1140bdfa07ac516436e4ebf120
Chad Nelson <chadbnelson@gmail.com>
2015-09-14 14:13:34 -0400

    Add transport id generator sql.

--- services/common/src/main/resources/db/postgresql/load_id_generators.sql
+++ services/common/src/main/resources/db/postgresql/load_id_generators.sql
@@ -40,6 +40,42 @@
  * initial value is '1').
  */
 
+-- TRANSPORT_NUMBER
+INSERT INTO id_generators
+    (csid, displayname, description, priority, last_generated_id, id_generator_state)
+  SELECT
+     'a19171fa-c33f-46cb-a952-0a112f076736',
+     'Transport Number',
+     'A unique number that identifies a transport.',
+     '9',
+     '',
+'<org.collectionspace.services.id.SettableIDGenerator>
+   <parts>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>TRA</initialValue>
+      <currentValue>TRA</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.YearIDGeneratorPart>
+      <currentValue></currentValue>
+    </org.collectionspace.services.id.YearIDGeneratorPart>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>.</initialValue>
+      <currentValue>.</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.NumericIDGeneratorPart>
+      <maxLength>6</maxLength>
+      <initialValue>1</initialValue>
+      <currentValue>-1</currentValue>
+    </org.collectionspace.services.id.NumericIDGeneratorPart>
+  </parts>
+</org.collectionspace.services.id.SettableIDGenerator>'
+  WHERE 'a19171fa-c33f-46cb-a952-0a112f076736' NOT IN
+        (
+        SELECT  csid
+        FROM    id_generators
+        );
+
+
 -- EXHIBITION_NUMBER
 
 INSERT INTO id_generators
