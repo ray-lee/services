7154f54538f73cb0405bcdce4c6c6390954e5c95
Ray Lee <rhlee@berkeley.edu>
2016-01-13 19:11:46 -0800

    CSPACE-6856: Change structured date vocab urns.

--- services/structureddate/src/main/java/org/collectionspace/services/structureddate/Certainty.java
+++ services/structureddate/src/main/java/org/collectionspace/services/structureddate/Certainty.java
@@ -1,12 +1,12 @@
 package org.collectionspace.services.structureddate;
 
 public enum Certainty {
-	AFTER         ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(after)'After'"),
-	APPROXIMATELY ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(approximate)'Approximate'"),
-	BEFORE        ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(before)'Before'"),
-	CIRCA         ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(circa)'Circa'"),
-	POSSIBLY      ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(possibly)'Possibly'"),
-	PROBABLY      ("urn:cspace:ucjeps.cspace.berkeley.edu:vocabularies:name(datecertainty):item:name(probably)'Probably'");
+	AFTER         ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(after)'After'"),
+	APPROXIMATELY ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(approximate)'Approximate'"),
+	BEFORE        ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(before)'Before'"),
+	CIRCA         ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(circa)'Circa'"),
+	POSSIBLY      ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(possibly)'Possibly'"),
+	PROBABLY      ("urn:cspace:herbarium.collectionspace.org:vocabularies:name(datecertainty):item:name(probably)'Probably'");
 	
 	private final String value;
 	
