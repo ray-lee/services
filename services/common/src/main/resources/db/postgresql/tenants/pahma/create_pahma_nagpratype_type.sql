84f10d65115fc36f828f87b77e7122dec3639ea4
Lam Voong <lkv@berkeley.edu>
2021-03-01 14:00:17 -0800

    CC-1501: Add .jrxml, payload, function, and type for pahmaRepatriationNAGPRAdenorm report

---
+++ services/common/src/main/resources/db/postgresql/tenants/pahma/create_pahma_nagpratype_type.sql
@@ -0,0 +1,29 @@
+-- Type used by utils.getnagpra(cocid varchar) to contain PAHMA NAGPRA Repatriation Compliance data
+-- for the pahmapahmaRepatriationNAGPRAdenorm.jrxml report.
+-- Must be created before function utils.getnagpra(cocid varchar).
+
+-- DROP TYPE nagpratype CASCADE;
+
+CREATE TYPE nagpratype AS (
+    cocsid varchar,
+    coid varchar,
+    pos integer,
+    objectNumber varchar,
+    sortableObjectNumber varchar,
+    nagpraInventoryName varchar,
+    nagpraCategory varchar,
+    graveAssocCode varchar,
+    repatriationNote varchar,
+    nagpraCulturalDetermination varchar,
+    nagpraDetermCulture varchar,
+    nagpraDetermType varchar,
+    nagpraDetermBy varchar,
+    nagpraDetermNote varchar,
+    nagpraReportFiled boolean,
+    nagpraReportFiledWith varchar,
+    nagpraReportFiledBy varchar,
+    nagpraReportFiledDate varchar,
+    nagpraReportFiledNote varchar,
+    reference varchar,
+    referenceNote varchar
+);
