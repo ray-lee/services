c5e9806b9dfd8b3ef02e602b197d5e5327b56e09
Richard Millet <richard.millet@berkeley.edu>
2024-02-01 12:10:46 -0800

    CSC-2274: Merging code from Lyrasis 8.x code base

---
+++ cspace-ui/build_js.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PACKAGE_NAME=$1
+BRANCH_NAME=$2
+CHECKOUT_DIR=$3
+OUTPUT_DIR=$4
+LIBRARY_NAME=$5
+SERVICE_LIBRARY_NAME=$6
+
+CODE_DIR=$CHECKOUT_DIR/$PACKAGE_NAME.js
+
+. "$HOME/.nvm/nvm.sh"
+
+rm -r $CODE_DIR
+git clone --branch $BRANCH_NAME --depth 1 https://github.com/collectionspace/$PACKAGE_NAME.js.git $CODE_DIR
+
+pushd $CODE_DIR
+COMMIT_HASH=`git rev-parse --short HEAD`
+npm install
+popd
+
+cp $CODE_DIR/dist/$LIBRARY_NAME.min.js "$OUTPUT_DIR/$LIBRARY_NAME@$COMMIT_HASH.min.js"
+
+if [ ! -z "$SERVICE_LIBRARY_NAME" ]
+then
+  cp $CODE_DIR/dist/$SERVICE_LIBRARY_NAME.min.js "$OUTPUT_DIR/$SERVICE_LIBRARY_NAME@$COMMIT_HASH.min.js"
+fi
